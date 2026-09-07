43bbc3298af76fb9e4f1ed2e00ea82fcf24f79cd
Sandu Speianu <sandu@Sandus-MacBook-Pro.local>
2026-09-07 20:22:21 +0100

    Added POST endpoint to create a new application

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Application(BaseModel):
+    role: str
+    company: str
+    location: str
+    salary: str
+    status: str
 
 applications = [
     {"role": "Backend Developer" , "company": "Sage", "location": "Newcastle", "salary":"£30,000", "status": "Ongoing"}, 
@@ -10,3 +18,8 @@ applications = [
 @app.get("/applications")
 async def get_applications():
     return applications
+
+@app.post("/applications")
+async def create_application(application: Application):
+    applications.append(application.dict())
+    return application
